6495f404317bbc431a92ec7488caf73a4fe81977
HSU-Jamie <azxcvb22915@gmail.com>
2022-03-24 10:06:43 +0800

    finally upload

---
+++ Function.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11de5c8e934eaa7d9ac31aa81d9defb79fe6da888a85c9e171bff928be4e2980
+size 3704
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc98fd08ae48ba9b9e75fc3ec6d3ec2c9ce9441cf495bae5f84fd5d77c895964
+size 485
