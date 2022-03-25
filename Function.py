4dce6af394c1fa1f4d885930e613cdcf19b12fbf
HSU-Jamie <azxcvb22915@gmail.com>
2022-03-25 11:27:08 +0800

    Close get_accument, Developing start_work

--- Function.py
+++ Function.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:726729a68162e71c60fd7c323db66fbf56550034be27c56aa762283218e16603
-size 4137
+oid sha256:6b1b57173ab0c61b936e69ec06cfdcf3c3939f3d0374bebb3c1c0cd1ab3f3d5d
+size 6006
---
+++ test_code.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bba150bd9760d1c381e0641c9aedf3bd741aa5920942ff4eaf766a69e759cbbb
+size 71
