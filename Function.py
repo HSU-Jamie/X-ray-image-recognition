2de7c5d8dbee755e18b209ee75397696ef1487b0
108318102 <azxcvb22915@gmail.com>
2022-03-27 23:10:26 +0800

    Fix Bug

--- Function.py
+++ Function.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b5171c8bdce9ce8118f033abda813ac5746cd862f4c8c8f6487034790fb4973
-size 8451
+oid sha256:02be6a181defb963649c9bc3f0dd0f0bfa9a6aaf4830a905b23e83c0eac29e04
+size 8482
