f61eda4dddf9dbbb915f027107f709a7a163846b
HSU-Jamie <azxcvb22915@gmail.com>
2022-03-29 11:25:15 +0800

    Close all function,starting debug

--- Function.py
+++ Function.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d70aed05da8f5af778009d8af6e43c2d08a0f769f667da14e3b56708ed7ba35f
-size 17393
+oid sha256:bf2f01d37b6847995e27a7717a06268a1806c7c3c7001a7e425c53da515632ff
+size 25659
