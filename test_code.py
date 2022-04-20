afa6a3c9fe96ee99cac52ca718a11a0c2ac0ec00
HSU-Jamie <azxcvb22915@gmail.com>
2022-04-20 11:52:00 +0800

    test code

--- test_code.py
+++ test_code.py
@@ -1,12 +1,12 @@
-import time
-
-def a(*args):
-    print(len(args))
-
-
-print(time.strftime('%Y%m%d%H%M-%S', time.localtime()))
-
-
-a([1, 2, 3, 4], [7777],'123456')
-
-
+from configparser import ConfigParser
+from pprint import pprint
+config_path = './config_ini.ini'
+config = {}
+f = ConfigParser()
+f.read(config_path)
+for section in f.sections():
+    config[section] = {}
+    for option in f.options(section):
+        config[section][option] = f.get(section, option)
+#pprint(config)
+pprint(config['FrameCapture']['capture0_state'])
